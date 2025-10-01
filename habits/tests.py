c5f70fececf1f3672de116468edb0b1bd4ad3a93
VsevolodLunev <126673517+VsevolodLunev@users.noreply.github.com>
2025-10-01 10:50:37 +0300

    cy.yaml

--- habits/tests.py
+++ habits/tests.py
@@ -1,9 +1,9 @@
-from django.test import TestCase
-from rest_framework import status
+# from django.test import TestCase
+# from rest_framework import status
 from rest_framework.test import APITestCase
 
-from habits.models import Habit
-from users.models import User
+# from habits.models import Habit
+# from users.models import User
 
 
 class HabitTestCase(APITestCase):
